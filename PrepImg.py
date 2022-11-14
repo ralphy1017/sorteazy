8ce12a7fbdaa8c7c02b2d3fd46e773af74c0507f
jssumme1 <97571686+jssumme1@users.noreply.github.com>
2022-11-13 18:50:58 -0700

    Add files via upload

--- PrepImg.py
+++ PrepImg.py
@@ -8,15 +8,17 @@ from reproject import reproject_interp  # https://reproject.readthedocs.io/en/st
 
 class PrepImg:
     
-    def __init__(self):
-        self.image_dir = 'N/A'
-        self.file = 'N/A'
-        self.imfile = 'N/A'
-        self.field = 'N/A'
+    ### INITIALIZE
+    ### Only inputs are image directory, file naming pattern, and field name
 
-        self.zp_sw = 28.0865
-        self.zp_lw = 28.0865
+    def __init__(self, image_dir, file, field):
+        # Initialize image locations and future names
+        self.image_dir = image_dir
+        self.file = file
+        self.field = field
+        self.imfile = f'{self.field}_*_sci.fits'
 
+        # Create empty data structures
         self.i2d_img = []
         self.sci_img = []
         self.wht_img = []
@@ -24,12 +26,16 @@ class PrepImg:
         
         # Ignore astropy FITSFixedWarning
         warnings.simplefilter('ignore')
-        
-    def get_i2d(self):
+
+        ### GET I2D IMAGES
+        ### Uses image_dir and file to find existing i2d images
+
         self.i2d_img = glob(os.path.join(self.image_dir, self.file))
         self.i2d_img.sort()
+
+        ### SPLIT I2D FILES
+        ### Creates 'sci' and 'wht' files from the i2d images
         
-    def split_i2d(self):
         for input_image in self.i2d_img:
             for i in range(len(input_image)):
                 if input_image[i] == 'f' and (input_image[i+4] == 'w' or input_image[i+4] == 'm') and input_image[i+1] in '0 1 2 3 4 5 6 7 8 9'.split():
@@ -55,7 +61,10 @@ class PrepImg:
                 print('SAVING', output_image)
                 fits.writeto(output_image, hdu_list[extension].data, header, overwrite=True)
                 
-    def get_zp(self):
+        ### GET ZEROPOINTS
+        ### Use pixel size in image header to calculate photometric zeropoints for SW and LW filters
+        ### If SW and LW images have the same pixel size, zp_sw == zp_lw
+
         zeropoints = []
         for file in self.sci_img:
             hdul = fits.open(file)
@@ -70,7 +79,9 @@ class PrepImg:
         self.zp_sw = max(zeropoints)
         self.zp_lw = min(zeropoints)
                 
-    def reproject_sci(self):
+        ### REPROJECT SCI AND WHT FILES
+        ### Having the images on the same pixel grid will be beneficial for SExtractor
+
         for image in self.sci_img + self.wht_img:
             ref_image = self.sci_img[0]
 
@@ -87,20 +98,16 @@ class PrepImg:
             fits.writeto(image, reprojected_data.astype('float32'), ref_header, overwrite=True)
             hdu.close()
 
-    def go(self, image_dir, file, field):
-        self.image_dir = image_dir
-        self.file = file
-        self.field = field
-        self.imfile = f'{field}_*_sci.fits'
-        
-        self.get_i2d()
-        self.split_i2d()
-        self.get_zp()
-        self.reproject_sci()
+        ### DONE SPLITTING AND REPROJECTING
+        ### Print
         
         print(f'Field: {self.field}\nImage Directory: {self.image_dir}\nImage File Pattern: {self.imfile}\n' +
               f'SW ZP: {self.zp_sw:.3f}\nLW ZP: {self.zp_lw:.3f}')
         
+    ### Method bkgsub
+    ### optionally subtract the background from the science images
+    ### size is the size of each square that background is measured and subtracted from
+
     def bkgsub(self, size=100):
         for image in self.sci_img:
             hdul = fits.open(image)
